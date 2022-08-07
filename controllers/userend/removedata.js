18a706f9c7dd41162823760001bd03b2a79cc782
Niloy173 <niloysaha173@gmail.com>
2022-08-07 13:05:00 +0600

    on back press few things done

--- controllers/userend/removedata.js
+++ controllers/userend/removedata.js
@@ -2,41 +2,74 @@ const fs = require("fs");
 const path = require("path");
 const createError = require("http-errors");
 
-const UPLAOD_FOLDER_FOR_COVER = path.join(
-  __dirname + "/../" + "/../public/coverPicture/"
-);
+const {
+  ProjectVerificationModel,
+} = require("../../models/ProjectVerificationSchema");
 
-const ATTACHMENT_PATH = path.join(
-  __dirname + "/../" + "/../public/attachments/"
-);
 // In this page when user's back pressing meaning
 // he/she wants to edit the project history
 // so remove the coverpicture
 // remove the attachment
-function RemoveData(req, res, next) {
-  fs.readdir(UPLAOD_FOLDER_FOR_COVER, (err, files) => {
-    // if (err) throw err;
+async function RemoveData(req, res, next) {
+  try {
+    const UPLAOD_FOLDER_FOR_COVER = path.join(
+      __dirname + "/../" + "/../public/coverPicture/"
+    );
 
-    for (let file of files) {
-      fs.unlink(path.join(UPLAOD_FOLDER_FOR_COVER, file), (err) => {
-        if (err) {
-          console.log(err.message);
-          createError(err);
-        }
-      });
-    }
-  });
+    const ATTACHMENT_PATH = path.join(
+      __dirname + "/../" + "/../public/attachments/"
+    );
 
-  fs.readdir(ATTACHMENT_PATH, (err, files) => {
-    for (let file of files) {
-      fs.unlink(path.join(ATTACHMENT_PATH, file), (err) => {
-        if (err) {
-          console.log(err.message);
-          createError(err);
+    const CurrentProjectData = await ProjectVerificationModel.findOne({
+      OwnerId: req.user.userId,
+    });
+
+    const AttachmentArray = CurrentProjectData.AttachmentsFileName;
+    const CoverPictureFilename = CurrentProjectData.CoverPictureFilename;
+    // delete the attachments & coverpicture file data
+    fs.readdir(UPLAOD_FOLDER_FOR_COVER, (err, files) => {
+      for (let file of files) {
+        if (file === CoverPictureFilename) {
+          fs.unlink(path.join(UPLAOD_FOLDER_FOR_COVER, file), (err) => {
+            if (err) {
+              console.log(err.message);
+            }
+          });
         }
-      });
+      }
+    });
+
+    if (AttachmentArray.length > 0) {
+      for (let file of AttachmentArray) {
+        fs.unlink(path.join(ATTACHMENT_PATH, file), (err) => {
+          if (err) {
+            console.log(err.message);
+          }
+        });
+      }
     }
-  });
+
+    const ProjectVerificationModelUpdate =
+      await ProjectVerificationModel.updateOne(
+        { OwnerId: req.user.userId },
+        {
+          $set: {
+            TargetAmount: "",
+            Validity: "",
+            Category: "",
+            CoverPicture: [],
+            Attachments: [],
+            CoverPictureFilename: "",
+            AttachmentsFileName: [],
+            MainStory: "",
+            StoryTitle: "",
+          },
+        },
+        { new: true, useFindAndModify: false }
+      );
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 module.exports = {
