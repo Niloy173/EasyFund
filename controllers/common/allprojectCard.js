47c4aee7b8cd7def48acfd81766596a7a1211d4d
Coder-Towhid <islamsmtowhidul@gmail.com>
2022-08-14 20:52:30 +0600

    bug fixed form responsive dark img banner

--- controllers/common/allprojectCard.js
+++ controllers/common/allprojectCard.js
@@ -7,11 +7,19 @@ async function GetMeAllProjectCard(req, res, next) {
       .select({})
       .limit(8)
       .sort("-createdAt");
+    const EngineeringData = await Project.find({ Category: "Engineering" })
+      .limit(4)
+      .sort("-createdAt");
+    const MedicalData = await Project.find({ Category: "Medical" })
+      .limit(4)
+      .sort("-createdAt");
 
     // console.log (ProjectData);
 
     res.status(200).render("home", {
       ProjectData,
+      EngineeringData,
+      MedicalData,
     });
   } catch (error) {
     // console.log(error);
